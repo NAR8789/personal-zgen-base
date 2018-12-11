65de841e7eb31a432507cc25e071194e322b2fd6
Xiao Fan <xiao.h.fan@gmail.com>
2018-12-11 07:47:10 -0800

    this comment no longer relevant

--- personal-zgen-base.zsh
+++ personal-zgen-base.zsh
@@ -13,9 +13,6 @@ zgen oh-my-zsh plugins/git
 zgen load djui/alias-tips
 zgen load starcraftman/zsh-git-prompt
 zgen load nar8789/personal-shell-configs
-# ^ currently required by personal-shell-configs, for my prompt setting.
-# TODO: remove this, or make zsh-git-prompt setup easier. (at the moment setup requires python or haskell or something
-# and potentially a compile step)
 
 # I think maybe these need to be included last..... we'll see
 zgen load zsh-users/zsh-completions
