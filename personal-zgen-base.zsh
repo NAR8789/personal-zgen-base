dc423f170afd42618e2722b5f536a0c35e5eefee
Xiao Fan <xiao@maven-labs.com>
2018-12-10 16:41:29 -0800

    zsh-git-prompt is currently required by personal-shell-configs

--- personal-zgen-base.zsh
+++ personal-zgen-base.zsh
@@ -11,6 +11,10 @@ zgen oh-my-zsh plugins/git
 
 zgen load djui/alias-tips
 zgen load nar8789/personal-shell-configs
+zgen load olivierverdier/zsh-git-prompt
+# ^ currently required by personal-shell-configs, for my prompt setting.
+# TODO: remove this, or make zsh-git-prompt setup easier. (at the moment setup requires python or haskell or something
+# and potentially a compile step)
 
 # I think maybe these need to be included last..... we'll see
 zgen load zsh-users/zsh-completions
