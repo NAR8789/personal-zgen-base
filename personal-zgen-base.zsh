4a8d9177dc6803db519bc91c6fddc3fc6aec737b
Xiao Fan <2258776+NAR8789@users.noreply.github.com>
2022-11-23 11:12:12 -0700

    only perform loads if zgen not yet saved

--- personal-zgen-base.zsh
+++ personal-zgen-base.zsh
@@ -1,25 +1,29 @@
-zgen load nar8789/oh-my-zsh-preamble
-CASE_SENSITIVE=true
-zgen oh-my-zsh
+if ! zgen saved; then
+  zgen load nar8789/oh-my-zsh-preamble
+  CASE_SENSITIVE=true
+  zgen oh-my-zsh
 
-# navigation plugins
-zgen oh-my-zsh plugins/dircycle
-zgen oh-my-zsh plugins/dirhistory
-zgen oh-my-zsh plugins/last-working-dir
-zgen oh-my-zsh plugins/fasd
-zgen oh-my-zsh plugins/shrink-path
+  # navigation plugins
+  zgen oh-my-zsh plugins/dircycle
+  zgen oh-my-zsh plugins/dirhistory
+  zgen oh-my-zsh plugins/last-working-dir
+  zgen oh-my-zsh plugins/fasd
+  zgen oh-my-zsh plugins/shrink-path
 
-zgen oh-my-zsh plugins/git
-zgen oh-my-zsh plugins/git-prompt
+  zgen oh-my-zsh plugins/git
+  zgen oh-my-zsh plugins/git-prompt
 
-zgen load djui/alias-tips
-zgen load nar8789/personal-shell-configs
+  zgen load djui/alias-tips
+  zgen load nar8789/personal-shell-configs
 
-# I think maybe these need to be included last..... we'll see
-zgen load zsh-users/zsh-completions
+  # I think maybe these need to be included last..... we'll see
+  zgen load zsh-users/zsh-completions
 
-zgen load zsh-users/zsh-autosuggestions
-# zgen load zsh-users/zsh-syntax-highlighting
-# unfortunately these currently interfere with yank-pop
+  zgen load zsh-users/zsh-autosuggestions
+  # zgen load zsh-users/zsh-syntax-highlighting
+  # unfortunately these currently interfere with yank-pop
 
-zgen load nar8789/zsh-intentional-chpwd
+  zgen load nar8789/zsh-intentional-chpwd
+else
+  CASE_SENSITIVE=true
+fi
